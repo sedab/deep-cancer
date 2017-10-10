4c89ff7dde2da07f2195f03e55235f1ee0ee0dd6
Joyce <joyce.wu@nyu.edu>
2017-10-10 14:09:55 -0400

    Modifying slurm file

--- run_job.sh
+++ run_job.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=jmw_cap
+#SBATCH --job-name=charrrr
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --time=47:00:00
 #SBATCH --mem=20GB
 #SBATCH --output=outputs/%A.out
 #SBATCH --error=outputs/%A.err
-#SBATCH --mail-user=jmw784@nyu.edu
 
 module purge
 module load python/intel/2.7.12
@@ -16,6 +15,6 @@ module load pillow/intel/4.0.0
 pip install http://download.pytorch.org/whl/cu80/torch-0.1.11.post5-cp27-none-linux_x86_64.whl --user --upgrade
 pip install torchvision --user --upgrade
 
-cd /scratch/jmw784/capstone/Charrrrtreuse/
+cd /scratch/eff254/Capstone/Charrrrtreuse/
 
 python -u model.py $1 > logs/$2.log
