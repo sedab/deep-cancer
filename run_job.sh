1a0bfd6e69dc78693c916f2819c8f43958c9f983
Joyce <joyce.wu@nyu.edu>
2017-10-27 00:46:23 -0400

    Updating slurm file

--- run_job.sh
+++ run_job.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #
 #SBATCH --job-name=charrrr
-#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=47:00:00
-#SBATCH --mem=20GB
+#SBATCH --time=23:00:00
+#SBATCH --mem=40GB
 #SBATCH --output=outputs/%A.out
 #SBATCH --error=outputs/%A.err
 
 module purge
 module load python3/intel/3.5.3
-module load pillow/python3.5/intel/4.2.1
-module load pytorch/python3.5/0.2.0_3
-module load torchvision/python3.5/0.1.9
+python3 -m pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl --user --upgrade
+python3 -m pip install torchvision --user --upgrade
+python3 -m pip install comet_ml --user
 
 cd /scratch/jmw784/capstone/Charrrrtreuse/
 
-python -u model.py $1 > logs/$2.log
+python -u train.py $1 --experiment $2 > logs/$2.log
