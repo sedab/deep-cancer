045e858231983237f77d7234dff3622e0c9fa4cd
Joyce <joyce.wu@nyu.edu>
2017-10-31 23:45:43 -0400

    Merging model into training script to improve traceability, adding option for nonlinearity, modified model

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import argparse
 import random
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
@@ -26,7 +27,7 @@ Options for training
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
 parser.add_argument('--imgSize', type=int, default=299, help='the height / width of the image to network')
-parser.add_argument('--nc', type=int, default=8, help='input image channels + concatenated info channels')
+parser.add_argument('--nc', type=int, default=3, help='input image channels (+ concatenated info channels if metadata = True)')
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0001, help='learning rate, default=0.0001')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam, default=0.5')
@@ -36,7 +37,10 @@ parser.add_argument('--model', default='', help="path to model (to continue trai
 parser.add_argument('--experiment', default=None, help='Where to store samples and models')
 parser.add_argument('--augment', action='store_true', help='Whether to use data augmentation or not')
 parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
+parser.add_argument('--metadata', action='store_true', help='Whether to use metadata (default is not)')
 parser.add_argument('--init', type=str, default='normal', help='initialization method (normal, xavier, kaiming)')
+parser.add_argument('--evalSize', type=int, default=2000, help='Number of samples to obtain validation loss on')
+parser.add_argument('--nonlinearity', type=str, default='relu', help='Nonlinearity to use (selu, prelu, leaky, relu)')
 opt = parser.parse_args()
 print(opt)
 
@@ -90,9 +94,9 @@ loaders = {}
 
 for dset_type in ['train', 'valid', 'test']:
     if dset_type == 'train' and opt.augment:
-        data[dset_type] = TissueData(root_dir, dset_type, transform = augment)
+        data[dset_type] = TissueData(root_dir, dset_type, transform = augment, opt.metadata)
     else:
-        data[dset_type] = TissueData(root_dir, dset_type, transform = transform)
+        data[dset_type] = TissueData(root_dir, dset_type, transform = transform, opt.metadata)
 
     loaders[dset_type] = torch.utils.data.DataLoader(data[dset_type], batch_size=opt.batchSize, shuffle=True)
     print('Finished loading %s dataset: %s samples' % (dset_type, len(data[dset_type])))
@@ -126,8 +130,47 @@ def weights_init(m):
         
         m.bias.data.fill_(0)
 
+# Define model
+class cancer_CNN(nn.Module):
+    def __init__(self, nc, imgSize, nonlinearity, ngpu):
+        super(cancer_CNN, self).__init__()
+        self.nc = nc
+        self.imgSize = imgSize
+        self.ngpu = ngpu
+
+        if nonlinearity == 'selu':
+            self.relu = nn.SELU()
+        elif nonlinearity == 'prelu':
+            self.relu = nn.PReLU()
+        elif nonlinearity == 'leaky':
+            self.relu = nn.LeakyReLU()
+        else:
+            self.relu = nn.ReLU()
+
+        self.init_dropout = nn.Dropout(p=0.2)
+        self.dropout = nn.Dropout(p=0.5)
+        self.conv1 = nn.Conv2d(nc, 64, 4, 2, 1, bias=True)
+        self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=True)
+        self.conv3 = nn.Conv2d(128, 256, 4, 2, 1, bias=True)
+        self.conv4 = nn.Conv2d(256, 128, 4, 2, 1, bias=True)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        # Three classes
+        self.linear = nn.Linear(4096, 3)
+
+    def forward(self, x):
+        x = self.init_dropout(self.bn1(self.relu(F.max_pool2d(self.conv1(x),2))))
+        x = self.dropout(self.bn2(self.relu(F.max_pool2d(self.conv2(x),2))))
+        x = self.dropout(self.bn3(self.relu(F.max_pool2d(self.conv3(x),2))))
+        x = x.view(x.size(0), -1)
+        x = F.softmax(self.linear(x))
+        return x
+
 # Create model objects
-model = cancer_CNN(nc, imgSize, ngpu)
+model = cancer_CNN(nc, imgSize, opt.nonlinearity, ngpu)
 model.apply(weights_init)
 model.train()
 
@@ -148,10 +191,23 @@ else:
     optimizer = optim.RMSprop(model.parameters(), lr = opt.lr)
 
 # Define evaluation function for an entire dataset (train, valid, or test)
-def evaluate(dset_type):
+def evaluate(dset_type, sample_size='full'):
+
+    """
+    Note: sample_size will be rounded up to be a multiple of the batch_size
+    of the dataloader.
+    """
+
+    if sample_size == 'full':
+        sample_size = len(data[dset_type])
+    elif not isinstance(sample_size, int):
+        raise ValueError("Amount should be 'full' or an integer")
+    elif sample_size > len(data[dset_type]):
+        raise ValueError("Amount cannot exceed size of dataset")    
 
     model.eval()
     loss = 0
+    num_evaluated = 0
 
     for img, label in loaders[dset_type]:
 
@@ -164,9 +220,11 @@ def evaluate(dset_type):
 
         loss += crossEntropy(model(eval_input), eval_label)
 
-    model.train()
+        num_evaluated += img.size(0)
 
-    return loss / len(data[dset_type])
+        if num_evaluated >= sample_size:
+            model.train()
+            return loss / num_evaluated
 
 print('Starting training')
 
@@ -203,13 +261,15 @@ for epoch in range(opt.niter+1):
                % (epoch, opt.niter, i, len(loaders['train']), train_loss.data[0]))
 
         if i % 200 == 0: # Can change how often to evaluate val set
-            val_loss = evaluate('valid')
-            experiment.log_metric("Validation loss", val_loss.data[0])
+
+            eval_size = int(opt.evalSize)
+            val_loss = evaluate('valid', sample_size=eval_size)
+            experiment.log_metric("Validation loss (%s samples)" % (eval_size), val_loss.data[0])
 
             print('[%d/%d][%d/%d] Validation Loss: %f'
                    % (epoch, opt.niter, i, len(loaders['valid']), val_loss.data[0]))
 
-    if epoch % 10 == 0:
+    if epoch % 5 == 0:
         torch.save(model.state_dict(), '{0}/epoch_{1}.pth'.format(opt.experiment, epoch))
 
 # Final evaluation
