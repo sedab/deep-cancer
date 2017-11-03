97b5b09ad1a3929e33e5977dd5d4df07f3b26b86
Joyce <joyce.wu@nyu.edu>
2017-11-03 14:56:05 -0400

    Adding resize to new transforms

--- new_transforms.py
+++ new_transforms.py
@@ -101,6 +101,32 @@ class Compose(object):
             img = t(img)
         return img
 
+class Resize(object):
+    """Resize the input PIL Image to the given size.
+    Args:
+        size (sequence or int): Desired output size. If size is a sequence like
+            (h, w), output size will be matched to this. If size is an int,
+            smaller edge of the image will be matched to this number.
+            i.e, if height > width, then image will be rescaled to
+            (size * height / width, size)
+        interpolation (int, optional): Desired interpolation. Default is
+            ``PIL.Image.BILINEAR``
+    """
+
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        assert isinstance(size, int) or (isinstance(size, collections.Iterable) and len(size) == 2)
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be scaled.
+        Returns:
+            PIL Image: Rescaled image.
+        """
+        return resize(img, self.size, self.interpolation)
+
 class RandomVerticalFlip(object):
     """Vertically flip the given PIL.Image randomly with a probability of 0.5."""
 
